6ad5e647354faf7cfb969eac461db454a7fa854f
Lucas Pena <lpena7@illinois.edu>
2016-10-13 14:58:26 -0500

    started defining kore in scala

--- kore/src/main/scala/org/kframework/definition/test.scala
+++ kore/src/main/scala/org/kframework/definition/test.scala
@@ -1,5 +1,7 @@
 package org.kframework.definition
 
+import org.kframework.attributes.Att
+
 import scala.collection.mutable.ListBuffer
 import org.kframework.kore._
 
@@ -18,14 +20,17 @@ class test {
   val Exp = ADT.SortLookup("Exp")
   val INT = Module("INT", Set(), Set(
     SyntaxSort(Int)
+    //Production(Int, Syntax(), Att())
   ))
 
   // module INT
-  //   syntax INTe
+  //   syntax Int
   // endmodule
 
-  def Att(atts: String*) = atts.foldLeft(org.kframework.attributes.Att())(_+_)
+
   import org.kframework.attributes.Att._
+
+  def Att(atts: String*) = atts.foldLeft(org.kframework.attributes.Att())(_+_)
 
   val IMP = Module("IMP", Set(INT), Set(
     Production(Exp, Syntax(Int, "+", Int), Att()),
@@ -39,5 +44,52 @@ class test {
   //   syntax Exp ::= Int "+" Int
   //   syntax Exp ::= Int "*" Int
   // endmodule
+
+  // module ATTRIBUTES
+  //   token Key ::= r"[a-z][A-Za-z\\-0-9]*"
+  //   syntax KeyList ::= Key
+  //   syntax KeyList ::= Key "," KeyList
+  //   syntax Attribute ::= Key
+  //   syntax Attribute ::= Key "(" KeyList ")"
+  //   syntax AttributeList ::= Attribute
+  //   syntax AttributeList ::= Attribute "," AttributeList
+  //   syntax Attributes ::= "[" AttributeList "]"
+  // endmodule
+
+  def Sort(s: String): ADT.SortLookup = ADT.SortLookup(s)
+
+  val Key = Sort("Key")
+  val KeyList = Sort("KeyList")
+  val Attribute = Sort("Attribute")
+  val AttributeList = Sort("AttributeList")
+  val Attributes = Sort("Attributes")
+
+  def regex(s: String): RegexTerminal = RegexTerminal("", s, "")
+
+  def Token(s: ADT.SortLookup, re: RegexTerminal): Production =
+    Production(s, Syntax(re), Att("token"))
+
+  case class BecomingProduction(sort: ADT.SortLookup, pis: ProductionItem*) {
+    def att(atts: String*): Production = Production(sort, pis, atts.foldLeft(Att())(_+_))
+  }
+
+  case class syntax(s: ADT.SortLookup) {
+    def ::= (pis: ProductionItem*): BecomingProduction = BecomingProduction(s, pis)
+  }
+
+  implicit def productionWithoutAttributes(bp: BecomingProduction) : Production =
+    Production(bp.sort, bp.pis, Att())
+
+  val ATTRIBUTES = Module("ATTRIBUTES", Set(), Set(
+    Token(Key, regex("[a-z][a-zA-Z\\-0-9]*")),
+    (syntax(KeyList) ::= (Key)).att("assoc"),
+    Production(KeyList, Syntax(Key), Att()),
+    Production(KeyList, Syntax(Key, ",", KeyList), Att()),
+    Production(Attribute, Syntax(Key), Att()),
+    Production(Attribute, Syntax(Key, "(", KeyList, ")"), Att()),
+    Production(AttributeList, Syntax(Attribute), Att()),
+    Production(AttributeList, Syntax(Attribute, ",", AttributeList), Att()),
+    Production(Attributes, Syntax("[", AttributeList, "]"), Att())
+  ))
 
 }
